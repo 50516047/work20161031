5e96ec722f33cf1b9e86a2e326cc6b47a23be187
50516047 <ramishi.br1v4h41rt.46841@ezweb.ne.jp>
2016-11-07 16:33:21 +0900

    work20161031

---
+++ src/combination/Combination_lib.java
@@ -0,0 +1,18 @@
+package combination;
+
+public class Combination_lib {
+	
+	public int getFactorial(int n){
+		int f = 1;
+		for(int i = n; i>=0; i--){
+			if(i==0) f*=1;
+			else f*=i;
+		}
+	    return f;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	}
+}
